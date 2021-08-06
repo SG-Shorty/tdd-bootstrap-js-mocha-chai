90bb6df896f3c82911ceadf4a4a6d45cef72288e
Sarah Schauppenlehner <sarah.schauppenlehner@scinteco.com>
2021-08-06 11:56:21 +0200

    snake test direction.

--- test/test.js
+++ test/test.js
@@ -1,18 +1,17 @@
-class Snake{
-    
+class Snake {
+    constructor() {}
 }
 
 const expect = require('chai').expect;
-
-describe('World', function() {
-    it('red', function() {
-        expect('foo').to.equal('foo')
-    })
-});
 
 describe('Snake', function() {
     it('exist', function() {
         let snake = new Snake();
         expect(snake).to.be.not.null;
+    })
+
+    it('should have a direction', function() {
+        let snake = new Snake();
+        expect(snake.getDirection()).to.be.null;
     })
 });
