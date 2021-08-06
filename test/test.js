fb0efcdd19bad15d3282c8373b229dca04df03ed
Shorty <patrick.scheibert@hotmail.com>
2021-08-06 11:29:00 +0200

    test snake exists

--- test/test.js
+++ test/test.js
@@ -2,6 +2,13 @@ const expect = require('chai').expect;
 
 describe('World', function() {
     it('red', function() {
-        expect('foo').to.equal('bar')
+        expect('foo').to.equal('foo')
+    })
+});
+
+describe('Snake', function() {
+    it('exist', function() {
+        let snake = new Snake();
+        expect(snake).to.be.not.null;
     })
 });
