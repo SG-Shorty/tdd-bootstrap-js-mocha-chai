d9ac730ea000bba9194ca85f42146561e63bf905
Thoma <thomas.horvath@ebcont.com>
2021-08-06 13:57:30 +0200

    Refactoring test

--- test/test.js
+++ test/test.js
@@ -4,18 +4,25 @@ Position = require('../src/Position.js');
 const expect = require('chai').expect;
 
 describe('Snake', function() {
+    
+    let snake;
+    
+    before(function() {
+        snake = new Snake();
+    });
+
     it('should have no direction on init', function() {
-        let snake = new Snake();
+
         expect(snake.getDirection()).to.be.null;
     })
 
     it('should have a position on init', function() {
-        let snake = new Snake();
+
         expect(snake.getHeadPosition()).to.deep.equal(new Position(0,0));
     })
 
     it('should not move without direction', function() {
-        const snake = new Snake();
+
         snake.update();
         expect(snake.getHeadPosition()).to.deep.equal(new Position(0,0));
     })
