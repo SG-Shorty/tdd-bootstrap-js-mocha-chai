92d8fa1dad45bdfac57632f7cfa226b9d414df62
Sarah Schauppenlehner <sarah.schauppenlehner@scinteco.com>
2021-08-06 14:01:15 +0200

    test recieve direction of snake.

--- test/test.js
+++ test/test.js
@@ -4,26 +4,28 @@ Position = require('../src/Position.js');
 const expect = require('chai').expect;
 
 describe('Snake', function() {
-    
+
     let snake;
-    
+
     before(function() {
         snake = new Snake();
     });
 
     it('should have no direction on init', function() {
-
         expect(snake.getDirection()).to.be.null;
     })
 
     it('should have a position on init', function() {
-
         expect(snake.getHeadPosition()).to.deep.equal(new Position(0,0));
     })
 
     it('should not move without direction', function() {
-
         snake.update();
         expect(snake.getHeadPosition()).to.deep.equal(new Position(0,0));
+    })
+
+    it('can recieve a new direction', function() {
+        snake.setDirection('north');
+        expect(snake.getDirection()).to.equal('north');
     })
 });
