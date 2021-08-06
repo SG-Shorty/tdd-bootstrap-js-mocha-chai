8fafda21e07fb0d5b912a76bd79fd27aea956c8b
Shorty <patrick.scheibert@hotmail.com>
2021-08-06 12:10:08 +0200

    remove exists test + rename direction test

--- test/test.js
+++ test/test.js
@@ -10,12 +10,7 @@ class Snake {
 const expect = require('chai').expect;
 
 describe('Snake', function() {
-    it('exist', function() {
-        let snake = new Snake();
-        expect(snake).to.be.not.null;
-    })
-
-    it('should have a direction', function() {
+    it('should have no direction on init', function() {
         let snake = new Snake();
         expect(snake.getDirection()).to.be.null;
     })
