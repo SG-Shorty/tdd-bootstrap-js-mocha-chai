47f3e7f4070070d447d4bb629ee8f514500da75a
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-07 00:12:50 +0200

    support esm

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
+import World from "../World.js";
+
 const expect = require('chai').expect;
 
 describe('World', function() {
     it('red', function() {
+        new World()
         expect('foo').to.equal('bar')
     })
 });
